f0d54507ff3ac7e2f579dba8b07cc2bdbb392163
Daniel Girardeau-Montaut <daniel.girardeau@gmail.com>
2022-04-16 10:40:02 +0200

    Glitch fix: use the closest point to interpolate the normal on the mesh triangle (for ICP with normal-based weights and a mesh as reference) instead of the point of the aligned cloud.

--- src/RegistrationTools.cpp
+++ src/RegistrationTools.cpp
@@ -603,7 +603,7 @@ ICPRegistrationTools::RESULT_TYPE ICPRegistrationTools::Register(	GenericIndexed
 					{
 						unsigned triIndex = static_cast<unsigned>(data.CPSetPlain->getPointScalarValue(i));
 						assert(triIndex >= 0 && triIndex < inputModelMesh->size());
-						inputModelMesh->interpolateNormals(triIndex, *data.cloud->getPoint(i), Nm);
+						inputModelMesh->interpolateNormals(triIndex, *data.CPSetPlain->getPoint(i), Nm);
 					}
 					else
 					{
